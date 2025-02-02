a56348dbcbebcc575fd2ceb2d66cc6139d07e178
Lennox Mwabonje <lenblazy@gmail.com>
2025-02-02 16:09:31 +0300

    Render index page path

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import PageWrapper from "./components/PageWrapper.jsx";
 import Home from "./components/pages/Home.jsx";
-import { BrowserRouter, Routes, Route } from "react-router";
+import {BrowserRouter, Routes, Route} from "react-router";
 
 
 function App() {
     // const [count, setCount] = useState(0)
 
     return (
-        <PageWrapper>
-            <BrowserRouter>
+        <BrowserRouter>
+            <PageWrapper>
                 <Routes>
-                    <Route path="/" element={<Home/>}/>
+                    <Route index element={<Home />} />
                 </Routes>
-            </BrowserRouter>
-        </PageWrapper>
+
+            </PageWrapper>
+        </BrowserRouter>
     )
 }
 
