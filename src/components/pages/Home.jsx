f430ec5d8eb9dcca0d72a84c9d3655bb85e471b6
Lennox Mwabonje <lenblazy@gmail.com>
2025-02-02 17:13:58 +0300

    Add common component services

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,14 +1,20 @@
 import Header from "../common/Header.jsx";
 import image from "../assets/img/header-bg.jpg";
+import Services from "../common/Services.jsx";
 
 const Home = () => {
     return (
+        <div>
+
         <Header title="Welcome To Our Studio!"
                 subTitle="It's Nice To Meet You"
                 showButton={true}
                 backgroundImage={image}
                 link={"/services"}
                 buttonText={"Tell Me More"} />
+
+            <Services />
+        </div>
     )
 }
 
